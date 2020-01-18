87cbd9d7c9e88f78cc049fdb4e89ac42d71c7d09
animevn <haanhth21@gmail.com>
2020-01-18 09:21:10 +0700

    add model

---
+++ app/src/main/java/com/haanhgs/recyclerglidedemo/Sport.java
@@ -0,0 +1,38 @@
+package com.haanhgs.recyclerglidedemo;
+
+public class Sport {
+
+    private String title;
+    private String info;
+    private int imgResource;
+
+    public Sport(String title, String info, int imgResource) {
+        this.title = title;
+        this.info = info;
+        this.imgResource = imgResource;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public int getImgResource() {
+        return imgResource;
+    }
+
+    public void setImgResource(int imgResource) {
+        this.imgResource = imgResource;
+    }
+}
